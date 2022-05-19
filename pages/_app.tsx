b441e09c87b863a3baddd9637835e08b287be91d
bertonnipaz <bertonnipaz@gmail.com>
2022-05-19 16:14:51 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// NEXTAUTH_SECRET=Hv+NrppBrjVVh5V/K2ikV/5Hk0i3gWpVWrIAeMvsKGg=
+// NEXTAUTH_URL=http://localhost:3000/api/auth/callback/reddit
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
