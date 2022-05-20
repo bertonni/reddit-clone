a235f7d399652305a37a6c0716966b315ab94c80
bertonnipaz <bertonnipaz@gmail.com>
2022-05-20 13:07:59 -0300

    configuring database and creating postBox component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import Header from '../components/Header';
+import PostBox from '../components/PostBox';
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-5xl my-7 mx-auto">
       <Head>
         <title>Reddit Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Header /> */}
+      {/* PostBox */}
+      <PostBox />
+      <div className='flex'>
+        {/* Feed */}
+      </div>
     </div>
   )
 }
